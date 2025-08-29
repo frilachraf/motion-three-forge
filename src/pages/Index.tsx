70a55b5b2e12b87611c881c2a80eacdcce0dc735
fril achraf <frilachraf.dev@gmail.com>
2025-08-29 03:40:49 +0100

    integrared supabase successfully

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,8 +2,20 @@ import ThreeScene from '@/components/ThreeScene';
 import HeroSection from '@/components/HeroSection';
 import TechStack from '@/components/TechStack';
 import SupabaseSection from '@/components/SupabaseSection';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 const Index = () => {
+  const [instruments, setInstruments] = useState([]);
+  useEffect(() => {
+    getInstruments();
+  }, []);
+  async function getInstruments() {
+    const { data } = await supabase.from("profiles").select();
+    setInstruments(data);
+  }
+
   return (
     <div className="min-h-screen bg-background relative">
       <ThreeScene />
